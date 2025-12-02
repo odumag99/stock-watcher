8211cdbd9c0c52017c7ed42614acde756d2c1714
Jaemin Kim <odumag99@gmail.com>
2025-12-03 02:05:50 +0900

    register_cron.sh cron 서비스 활성화

--- register_cron.sh
+++ register_cron.sh
@@ -3,4 +3,8 @@
 # Register the cron job
 # every hour execute
 # docker compose run --rm watcher
+
+sudo systemctl enable cron
+sudo systemctl start cron
+
 echo "0 * * * * docker compose -f ${PWD}/docker-compose.yaml run --rm watcher >> ${PWD}/logs 2>&1" | crontab -
