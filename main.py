3742a4d3be08719f935cfd8e5d86aa63c8199fb4
Jaemin Kim <odumag99@gmail.com>
2025-12-03 21:29:56 +0900

    tesla_sell_watcher 추가

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from stock_watcher.stock_watch_manager import StockWatchManager
 
 from watchers.google_watcher import GoogleWatcher
 from watchers.samsung_electronics_watcher import SamsungElectronicsWatcher
+from watchers.tesla_buy_watcher import TeslaBuyWatcher
+from watchers.tesla_sell_watcher import TeslaSellWatcher
 
 async def main():
     manager = StockWatchManager(
-        [GoogleWatcher, SamsungElectronicsWatcher]
+        [GoogleWatcher, SamsungElectronicsWatcher, TeslaBuyWatcher, TeslaSellWatcher]
     )
 
     await manager.dispatch()
--- watchers/tesla_sell_watcher.py
+++ watchers/tesla_sell_watcher.py
@@ -7,7 +7,7 @@ from stock_watcher.logger import logger
 
 KST = timezone(timedelta(hours=9))
 
-class TeslaBuyWatcher(Watcher):
+class TeslaSellWatcher(Watcher):
     def __init__(self):
         self.stock_symbol: str = "TSLA"
         self.korean_name: str = "테슬라"
@@ -20,13 +20,13 @@ class TeslaBuyWatcher(Watcher):
         
         if should_notify:
             message = f"""\
-[Tesla 매수 알림]
-매수 권장 타이밍입니다.
+[Tesla 매도 알림]
+매도 권장 타이밍입니다.
 현재 가격: {current_price:,.2f}
 기준 가격: {threshold_price:,.2f}
 알림 여부: {should_notify}"""
         else:
-            message = f"[Tesla 매수 알림] 현재 가격 - {current_price:,.2f}, 기준 가격 - {threshold_price:,.2f}, 알림 여부 - False"
+            message = f"[Tesla 매도 알림] 현재 가격 - {current_price:,.2f}, 기준 가격 - {threshold_price:,.2f}, 알림 여부 - False"
         
         return WatchResult(
             datetime=datetime.now(tz=KST),
@@ -40,7 +40,7 @@ class TeslaBuyWatcher(Watcher):
         return await PriceGetter(self.stock_symbol).get_recent_price()
     
     async def _get_threshold_price(self) -> float:
-        base_price = 407.55
+        base_price = 410
         baseday = datetime.strptime("2025-12-01", "%Y-%m-%d")
         now = datetime.now()
         diff_days = (now - baseday).days
